57781035aa6525bf874d1cf24a37bb054c896530
Joonas Kylmälä <joonas.kylmala@iki.fi>
2020-06-29 08:06:03 -0400

    Move common configurations from i9300 to midas-common
    Signed-off-by: Joonas Kylmälä <joonas.kylmala@iki.fi>

--- prevent_suspend.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-mkdir -p /storage/sys/power/
-touch /storage/sys/power/state
-mount -o bind /storage/sys/power/state /sys/power/state
--- resize2fs_partitions.sh
+++
@@ -1,2 +0,0 @@
-#!/system/bin/sh
-resize2fs /dev/block/platform/soc/12550000.mmc/by-name/USERDATA

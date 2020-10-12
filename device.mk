9a9de89582b0212256f1a323a3084098e361ad7c
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-10-12 22:04:28 +0200

    device.mk: use LOCAL_PATH whenever possible
    This shorten the lines and makes this repository more position
    independent.
    
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- device.mk
+++ device.mk
@@ -20,8 +20,8 @@ PRODUCT_PACKAGES += \
     libsamsung-ipc \
 
 PRODUCT_COPY_FILES += \
-    device/samsung/i9300/fstab.smdk4x12:root/fstab.smdk4x12 \
-    device/samsung/i9300/init.smdk4x12.rc:root/init.smdk4x12.rc \
+    $(LOCAL_PATH)/fstab.smdk4x12:root/fstab.smdk4x12 \
+    $(LOCAL_PATH)/init.smdk4x12.rc:root/init.smdk4x12.rc \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml \
     frameworks/av/media/libstagefright/data/media_codecs_sw.xml:system/etc/media_codecs.xml \

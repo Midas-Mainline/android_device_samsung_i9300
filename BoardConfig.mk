38a7238ec12b65d6557432e8237759f6f61f4ff0
Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>
2020-11-24 18:17:04 +0100

    Build the kernel
    Signed-off-by: Denis 'GNUtoo' Carikli <GNUtoo@cyberdimension.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,9 @@
 
 -include device/samsung/midas-common/BoardConfigCommon.mk
 
+# Kernel
+BOARD_DTB_IMAGE_NAME := exynos4412-i9300.dtb
+
 # Recover
 TARGET_RECOVERY_FSTAB := device/samsung/i9300/fstab.smdk4x12
 TARGET_RECOVERY_PIXEL_FORMAT := ABGR_8888
